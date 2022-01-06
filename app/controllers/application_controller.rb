af2305892b98574bd6d625190ee4db29a58959e8
HMTanzeel <mtanzeel6363@gmail.com>
2022-01-06 16:07:45 +0500

    before filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  private
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      user.find_by(id: session[:current_user_id])
+  end
 end
---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,11 @@
+class ClientsController < ApplicationController
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      # ...
+    else
+      flash.now[:error] = "Could not save client"
+      render action: "new"
+    end
+  end
+end
